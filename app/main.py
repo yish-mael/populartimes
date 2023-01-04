63e1ebeb3ab55ba01ef015bb8f218665a2376354
Jude Nwankpa <judenwankpa@Judes-MacBook-Air.local>
2023-01-03 23:05:04 -0600

    init message

--- app/main.py
+++ app/main.py
@@ -15,7 +15,24 @@ async def data():
     results = await request()
     return { "data": results}
 
-# @app.get("/api/data/{id}")
+
+google_place_url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=amoeba&types=establishment&location=37.76999%2C-122.44696&radius=500&key=AIzaSyCFRoQAMWl1TXH5la1csQdf34jYJ8OYxm8"
+
+
+
+async def placerequest():
+    payload={}
+    headers = {}
+    #response = requests.get(google_place_url, headers=headers, data=payload)
+    response = requests.get(google_place_url)
+    return response.text
+    
+@app.get("/api/google/place")
+async def place():
+    results = await placerequest()
+    return {"data": results}
+
+
 # def zip_by_id(id:int) -> dict:
 #     results = [zip for zip in ]
 
